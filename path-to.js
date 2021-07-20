ab673f3bf9761efe0b5132edfd0c3cba3153b342
SunsetSmores <87417463+SunsetSmores@users.noreply.github.com>
2021-07-20 15:52:50 -0600

    Add files via upload

---
+++ path-to.js
@@ -0,0 +1,23 @@
+var seconds = 1; // seconds 
+var foo; // variable for clearInterval() function
+
+function redirect() {
+    document.location.href = 'http://sunsetsmores.com';
+}
+
+function updateSecs() {
+    
+    seconds--;
+    if (seconds == -1) {
+        clearInterval(foo);
+        redirect();
+    }
+}
+
+function countdownTimer() {
+    foo = setInterval(function () {
+        updateSecs()
+    }, 1000);
+}
+
+countdownTimer();
